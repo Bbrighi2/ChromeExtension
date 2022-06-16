5fb998b82eab54061f7e8bb95a0d74f6116abc60
Jim White <Jim.White@me.com>
2022-06-16 18:44:45 -0400

    Version 1.5 with Border inset fix

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ window.onload = () => {
   const border = document.createElement('div');
   border.classList.add('border-frame');
   html.prepend(border);
-  css.appendChild(
-    document.createTextNode(`
+  css.appendChild(document.createTextNode(`
       .border-frame {
       position: fixed;
       pointer-events: none;
@@ -24,8 +23,23 @@ window.onload = () => {
       border-image-slice: 10%;
       border-image-repeat: stretch, round;
     }
-      `)
-  );
+
+    html {
+      border: 20px transparent;
+      margin: 20px;
+      position: absolute;
+      bottom: 0;
+      top: 0;
+      left: 0;
+      right: 0;
+      }
+
+      .c93Gbe{
+        positon: relative;
+        top: -20px;
+      }
+
+      `));
   document.getElementsByTagName('head')[0].appendChild(css);
   console.log('Hello World!!');
 };
