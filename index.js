79913ad0043a7eeb6417b800ebb2cabbef26b52e
Brandon Brighi <brandon.erosenvironmental@gmail.com>
2022-06-16 13:59:51 -0700

    border stays on fullscreen and new popup feature

--- index.js
+++ index.js
@@ -4,27 +4,28 @@
 window.onload = () => {
   const myRainbow = chrome.extension.getURL('/RainbowSinebow22px50msLtoR.gif');
   const css = document.createElement('style');
-  css.appendChild(document.createTextNode(`
-      html {
+  const html = document.querySelector('html');
+  const border = document.createElement('div');
+  border.classList.add('border-frame');
+  html.prepend(border);
+  css.appendChild(
+    document.createTextNode(`
+      .border-frame {
+      position: fixed;
+      pointer-events: none;
       border: 20px solid;
       border-image-source: url(${myRainbow});
-      z-index: 999;
-      position: absolute;
-      scroll-padding-bottom: 80px;
-      bottom: 0;
-      top: 0;
+      z-index: 99999;
+      bottom: 0vh;
+      top: 0vh;
       left: 0;
       right: 0;
       border-image-width: 20px;
       border-image-slice: 10%;
       border-image-repeat: stretch, round;
-      }
-
-      .c93Gbe{
-        positon: relative;
-        top: -20px;
-      }
-      `));
+    }
+      `)
+  );
   document.getElementsByTagName('head')[0].appendChild(css);
   console.log('Hello World!!');
 };
